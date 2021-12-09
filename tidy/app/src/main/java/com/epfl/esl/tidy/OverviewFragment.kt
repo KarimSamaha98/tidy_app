7f291f876151b151e13e7dac1fd8fb1be32da15f
Alec Flowers <alec.flowers00@gmail.com>
2021-12-09 16:49:59 +0100

    feat: recycler view grid view & pulling data from firebase

--- tidy/app/src/main/java/com/epfl/esl/tidy/OverviewFragment.kt
+++ tidy/app/src/main/java/com/epfl/esl/tidy/OverviewFragment.kt
@@ -7,8 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.epfl.esl.tidy.databinding.FragmentOverviewBinding
+import com.google.firebase.database.*
 
 class OverviewFragment : Fragment() {
 
@@ -18,6 +20,12 @@ class OverviewFragment : Fragment() {
 
     private lateinit var viewModel: OverviewViewModel
     private lateinit var binding : FragmentOverviewBinding
+    val tempID = 0
+
+    val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    val roomRef: DatabaseReference = database.getReference("Space_IDs")
+    var roomList2 : ArrayList<String> = arrayListOf()
+
     private val roomList : ArrayList<String> = arrayListOf("Living Room", "Dining Room", "Kitchen", "Bathroom")
 
     override fun onCreateView(
@@ -28,11 +36,29 @@ class OverviewFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_overview,
             container, false)
 
-        binding.recyclerViewItems.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        binding.recyclerViewItems.layoutManager = GridLayoutManager(activity, 2)
 
-        val roomAdapter = context?.let{ RoomAdapter(context=it, items = roomList)}
+//        TODO move to view model
+        val roomListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(space in snapshot.children) {
+                    if (space.child("Space_ID").getValue(Int::class.java)!! == tempID) {
+                        for(rooms in space.child("Rooms").children) {
+                            val room = rooms.child("Attribute").getValue(String::class.java)!!
+                            roomList2.add(room)
+                        }
+                    }
+                }
+                val roomAdapter = context?.let{ RoomAdapter(context=it, items = roomList2)}
 
-        binding.recyclerViewItems.adapter = roomAdapter
+                binding.recyclerViewItems.adapter = roomAdapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        }
+        roomRef.addListenerForSingleValueEvent(roomListener)
+
 
         return binding.root
     }
